9ba02505dd4b4ff19eba52f1f1529a799c06f000
charrymaks <charitymakau0@gmail.com>
2023-09-07 14:10:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -a program that prints a line with put function
+ * Return: 0
+ */
+int main(void)
+{
+puts ("/""Programming is like building a multilingual puzzle");
+Return (0)
+}
