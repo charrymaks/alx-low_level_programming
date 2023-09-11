30da7e4aebb8ae3373a5f4705d9d3b95ccbea604
charrymaks <charitymakau0@gmail.com>
2023-09-11 10:01:15 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include <sdtio.h>
+#include <ctype.h>
 
 /**
- * main - prints the last digit of the random
- * number stored in the variable n
- * Return: Always 0 (Success)
+ * main - entry point
+ *
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
-	return (0);
-
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase)
+lowerCase += 1;
+}
+putchar('\n');
+return (0)
 }
