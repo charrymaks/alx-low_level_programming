bd7bef0d03d64e4ba89097d5d0bccea1f2f1a574
charrymaks <charitymakau0@gmail.com>
2023-09-19 15:30:42 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,15 @@
 
 /**
  * swap_int - take in two variables and swaps them
- *
- * return: always 0
+ * @a: value 1
+ * @b: value 2
+ * return: void
  */
 
 void swap_int(int *a, int *b)
 {
-int swap;
-b = *a;
-*a = change;
+int c;
+c = *a;
+*a = *b;
+*b =  c;
 }
